63e7d0061b6b6e043cf1ed25cd7c7fa827cfe667
rajanm <rajan.manickavasagam@gmail.com>
2015-09-20 13:09:33 +0530

    modified folder structure

---
+++ ElasticsearchWebApiSample/Models/SampleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElasticsearchWebApiSample.Models
+{
+    public class SampleConfiguration
+    {
+        public SampleConfiguration()
+        {
+            //System.Configuration.Configuration SampleWebConfig =
+            //            System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(null);
+            //System.Configuration.KeyValueConfigurationElement ElasticsearchServer =
+            //    SampleWebConfig.AppSettings.Settings["ElasticsearchServer"];
+            //if (ElasticsearchServer != null)
+            //        ElasticsearchServerHost = ElasticsearchServer.Value;
+            ElasticsearchServerHost = System.Configuration.ConfigurationManager.AppSettings["ElasticsearchServer"];
+
+        }
+
+        public string ElasticsearchServerHost { get; set; }
+    }
+}
